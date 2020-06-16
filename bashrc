9d5fd2b112bdcac879ee099ded08dfe9d16a337b
David Luo <me@dsluo.dev>
2020-06-15 23:57:35 -0400

    add bash powerline

--- bashrc
+++ bashrc
@@ -28,3 +28,11 @@ if [ -x "$(command -v xclip)" ]
 then
     alias cb="xclip -selection clipboard"
 fi
+
+if [ -f /usr/share/powerline/bindings/bash/powerline.sh ]
+then
+    powerline-daemon -q
+    POWERLINE_BASH_CONTINUATION=1
+    POWERLINE_BASH_SELECT=1
+    . /usr/share/powerline/bindings/bash/powerline.sh
+fi
--- setup.sh
+++ setup.sh
@@ -6,11 +6,16 @@ declare -A configs=(
     ["bashrc"]=".bashrc"
     ["bash_profile"]=".bash_profile"
     ["gitconfig"]=".gitconfig"
+    ["powerline"]=".config/powerline"
 )
 
 for config in "${!configs[@]}"
 do
     echo "Linking $config."
+    if [ -d $PWD/$config ]
+    then
+        mkdir -p $(dirname $HOME/${configs[$config]})
+    fi
     ln -s -i $PWD/$config $HOME/${configs[$config]}
 done
 
