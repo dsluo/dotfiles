3edb1176bdde5e27c711c30e0b52aa05aaa8c299
dsluo <me@dsluo.dev>
2026-06-26 21:59:45 -0400

    nvim, lmstudio

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,3 +5,8 @@ if status is-interactive
 end
 $HOME/.local/bin/mise activate fish | source
 set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx EDITOR nvim
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /Users/dsluo/.lmstudio/bin
+# End of LM Studio CLI section
