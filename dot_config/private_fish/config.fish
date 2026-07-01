6d5703e6ed5a35bf04faea092e9f92f0ff7b9e5a
dsluo <me@dsluo.dev>
2026-06-30 22:37:26 -0400

    aliases

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     alias vim nvim
     alias lg lazygit
+    alias tf tofu
+    alias tg terragrunt
+    alias kubecolor kubectl
+    alias k kubecolor
 end
 $HOME/.local/bin/mise activate fish | source
 set -gx XDG_CONFIG_HOME $HOME/.config
