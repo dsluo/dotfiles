9be9dff8e80147a04d077f9c65358559d1e5f8d3
dsluo <me@dsluo.dev>
2026-09-01 09:57:11 -0400

    orbstack

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -22,3 +22,7 @@ set -gx EDITOR nvim
 # Added by LM Studio CLI (lms)
 set -gx PATH $PATH /Users/dsluo/.lmstudio/bin
 # End of LM Studio CLI section
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
